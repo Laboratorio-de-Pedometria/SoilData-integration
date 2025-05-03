2556e616bf80eee1460ac8b3a71158ee096b4d79
samuel-rosa <alessandrosamuelrosa@gmail.com>
2025-05-03 11:13:39 -0300

    Refactor data merging script: enhance package loading, improve directory checks, and update output filenames

--- src/13_merge_curated_data.R
+++ src/13_merge_curated_data.R
@@ -8,12 +8,44 @@ rm(list = ls())
 # Install and load required packages
 if (!require("data.table")) {
   install.packages("data.table")
+  library(data.table)
+}
+if (!require("sf")) {
+  install.packages("sf")
+  library(sf)
+}
+if (!require("geobr")) {
+  install.packages("geobr")
+  library(geobr)
 }
 
 # Source helper functions
-source("src/00_helper_functions.r")
+source("src/00_helper_functions.R")
+
+# Download Brazilian state boundaries
+brazil <- geobr::read_state()
 
 # Read curated data
+dir_path <- "~/ownCloud/SoilData"
+# Check if the directory exists
+if (!dir.exists(dir_path)) {
+  stop("Directory does not exist: ", dir_path)
+} else {
+  curated_path <- list.files(
+    path = path.expand(dir_path), pattern = "^ctb[0-9]{4}\\.csv$",
+    full.names = TRUE, recursive = TRUE
+  )
+  length(curated_path) # 28 datasets
+  print(curated_path)
+}
+
+# Read all files and store them in a list
+curated_list <- lapply(curated_path, function(x) {
+  data.table::fread(x)
+})
+
+# rbind all datasets keeping only the matching columns
+# Target columns
 read_cols <- c(
   "dataset_id", "observacao_id",
   "data_ano",
@@ -25,67 +57,66 @@ read_cols <- c(
   "profund_sup", "profund_inf",
   "carbono", "ctc", "ph", "argila", "silte", "areia", "terrafina", "dsi"
 )
-curated_path <- list.files(
-  path = path.expand("~/ownCloud/SoilData"),
-  pattern = "^ctb[0-9]{4}\\.csv$",
-  full.names = TRUE, recursive = TRUE
-)
-length(curated_path) # 28 datasets
-print(curated_path)
-
-# Read all files and store them in a list
-curated_list <- lapply(curated_path, function(x) {
-  data.table::fread(x)
-})
-
-# rbind all datasets keeping only the matching columns
 curated_data <- data.table::rbindlist(curated_list, fill = TRUE)
 curated_data <- curated_data[, ..read_cols]
 curated_data[, id := paste0(dataset_id, "-", observacao_id)]
 summary_soildata(curated_data)
-# Layers: 9818
-# Events: 3654
+# Layers: 9900
+# Events: 3677
 # Georeferenced events: 3357
 
 # Read SoilData data processed in the previous script
-soildata <- data.table::fread("data/12_soildata_soc.txt", sep = "\t")
+soildata <- data.table::fread("data/12_soildata.txt", sep = "\t")
 summary_soildata(soildata)
-# Layers: 52260
-# Events: 15175
-# Georeferenced events: 12045
+# Layers: 52294
+# Events: 15209
+# Georeferenced events: 12079
 
-# Identify datasets missing the carbon content
-# na_carbon <- soildata[is.na(carbono), .N, by = dataset_id]
-# na_carbon[N > 50]
-# ctb <- "ctb0039"
-# soildata[dataset_id == ctb, .(dataset_id, observacao_id, camada_nome, carbono)]
+# Check spatial distribution before merging data
+soildata_sf <- soildata[!is.na(coord_x) & !is.na(coord_y)]
+soildata_sf <- sf::st_as_sf(soildata_sf, coords = c("coord_x", "coord_y"), crs = 4326)
+file_path <- "res/fig/13_spatial_distribution_before_curated_data.png"
+png(file_path, width = 480 * 3, height = 480 * 3, res = 72 * 3)
+plot(brazil["code_state"],
+  col = "gray95", lwd = 0.5, reset = FALSE,
+  main = "Spatial distribution of SoilData before merging curated data"
+)
+plot(soildata_sf["estado_id"], cex = 0.3, add = TRUE, pch = 20)
+dev.off()
 
 # Merge curated data with SoilData
+# Adjust column names
 data.table::setnames(soildata, old = "data_coleta_ano", new = "data_ano")
 data.table::setnames(soildata, old = "coord_datum_epsg", new = "coord_datum")
+# Filter out duplicated datasets
 curated_ctb <- curated_data[, unique(dataset_id)]
 soildata <- soildata[!dataset_id %in% curated_ctb]
 summary_soildata(soildata)
-# Layers: 51044
-# Events: 14761
-# Georeferenced events: 11633
+# Layers: 51078
+# Events: 14795
+# Georeferenced events: 11667
+# Merge curated data with SoilData
 soildata <- rbind(soildata, curated_data, fill = TRUE)
 summary_soildata(soildata)
-# Layers: 60862
-# Events: 18415
-# Georeferenced events: 14990
+# Layers: 60978
+# Events: 18472
+# Georeferenced events: 15024
 
-# Check spatial distribution
-if (FALSE) {
-  x11()
-  soildata_sf <- soildata[!is.na(coord_x) & !is.na(coord_y)]
-  soildata_sf <- sf::st_as_sf(soildata_sf, coords = c("coord_x", "coord_y"), crs = 4326)
-  plot(soildata_sf["estado_id"], cex = 0.5)
-}
+# Check spatial distribution after merging curated data
+soildata_sf <- soildata[!is.na(coord_x) & !is.na(coord_y)]
+soildata_sf <- sf::st_as_sf(soildata_sf, coords = c("coord_x", "coord_y"), crs = 4326)
+file_path <- "res/fig/13_spatial_distribution_after_curated_data.png"
+png(file_path, width = 480 * 3, height = 480 * 3, res = 72 * 3)
+plot(brazil["code_state"],
+  col = "gray95", lwd = 0.5, reset = FALSE,
+  main = "Spatial distribution of SoilData after merging curated data"
+)
+plot(soildata_sf["estado_id"], cex = 0.3, add = TRUE, pch = 20)
+dev.off()
 
 ####################################################################################################
 # Export cleaned data
 # Number of datasets
 length(soildata[, unique(dataset_id)])
-# 262 datasets
-data.table::fwrite(soildata, "data/13_soildata_soc.txt", sep = "\t")
+# 263 datasets
+data.table::fwrite(soildata, "data/13_soildata.txt", sep = "\t")
