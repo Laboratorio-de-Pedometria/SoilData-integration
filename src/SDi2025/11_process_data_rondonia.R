fc4550635e695cac3e6c49ce35fb5924d9eeb56d
Alessandro Rosa <alessandrosamuelrosa@gmail.com>
2026-09-20 19:05:08 -0300

    Refactor layer merging process and add overlap join for layers from ctb0033 and ctb0034

--- src/SDi2025/11_process_data_rondonia.R
+++ src/SDi2025/11_process_data_rondonia.R
@@ -406,9 +406,6 @@ layer34[
 # We will keep the remaining layers with thickness different from 5 cm for a 
 # future check. One thing that we noticed is that a few cases are Latossolos.
 
-
-
-
 # Merge the two datasets
 sapply(list(layer33, layer34), nrow)
 # 10779 and 419 layers
@@ -416,10 +413,17 @@ sapply(list(layer33, layer34), nrow)
 # Apply overlap joint to merge layers from ctb0033 and ctb0034
 # ctb0033 has data on chemical soil properties measured over soil layers
 # corresponding to entire or part of pedological horizons. ctb0034 has data on
-# physical soil properties measured over thin layers (5 cm) that do not
-# necessarily correspond to the layers sampled in ctb0033.
-data.table::setkey(layer33, evento_id_febr, camada_id_febr)
+# physical soil properties measured over thin layers (most of them are 5 cm
+# thick) that do not necessarily correspond to the layers sampled in ctb0033.
+data.table::setkey(layer34, evento_id_febr, profund_sup, profund_inf)
+layerRO_overlap <- data.table::foverlaps(layer33, layer34,
+  by.x = c("evento_id_febr", "profund_sup", "profund_inf"),
+  by.y = c("evento_id_febr", "profund_sup", "profund_inf"),
+  type = "within", mult = "all"
+)
+write.csv(layerRO_overlap, "data/11_layerRO_overlap.csv", row.names = FALSE)
 
+nrow(layerRO_overlap)
 
 # We need an overlap join between the layers from ctb0033 and ctb0034 to merge them!!!
 
